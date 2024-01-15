9066b5809dc1ad750679cb1154844deca608b42a
jesus <chuchodavids@Jesuss-MacBook-Pro.local>
2024-01-15 14:21:12 -0600

    Added extras

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    {
+      "stevearc/conform.nvim",
+      opts = {},
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          -- Conform will run multiple formatters sequentially
+          python = { "isort", "black" },
+          -- Use a sub-list to run only the first available formatter
+          javascript = { { "prettierd", "prettier" } },
+          terraform = { { "tflint" } },
+        },
+      }),
+    },
+  },
+}
