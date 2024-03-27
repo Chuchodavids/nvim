da55f7117349adc61677b6b9d6587f6ea9f3f4fc
jesus <jesus.anaya@telco.in>
2024-03-27 10:59:58 -0500

    added diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gz", "<cmd>DiffviewOpen<cr>", desc = "diffview Open" },
+    { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "diffview File history" },
+    { "<leader>gx", "<cmd>DiffviewClose<cr>", desc = "diffview close" },
+  },
+}
